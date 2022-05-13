5f52770db67b4f52b2c94a29042ce497d0f57b91
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-05-13 13:47:58 +0300

    .

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://rechauffementclimatique.netlify.app/';
+$config['base_url'] = 'https://rechauffementclimatiques.000webhostapp.com/';
 
 /*
 |--------------------------------------------------------------------------
