4ee64893c4e3b82a54093e18735c83934a4150d6
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-05-14 23:45:02 +0300

    Update FirstModel.php

--- application/models/FirstModel.php
+++ application/models/FirstModel.php
@@ -12,6 +12,48 @@ class FirstModel extends CI_Model
         return $tab;
     }
 
+    public function getQuestionById($id){
+        $query="SELECT * FROM Question WHERE id=%s";
+        $sprint = sprintf($query, $this->db->escape($id));
+        $query = $this->db->query($sprint);
+        $tab = array();
+        foreach ($query->result_array() as $row) {
+            $nom = $row;
+            $tab[] = $nom;
+        }
+        return $tab;
+    }
+
+    public function getCause(){
+        $query=$this->db->query('SELECT * FROM Cause');
+        $tab = array();
+        foreach ($query->result_array() as $row) {
+            $nom = $row;
+            $tab[] = $nom;
+        }
+        return $tab;
+    }
+
+    public function getConsequence(){
+        $query=$this->db->query('SELECT * FROM Consequence');
+        $tab = array();
+        foreach ($query->result_array() as $row) {
+            $nom = $row;
+            $tab[] = $nom;
+        }
+        return $tab;
+    }
+
+    public function getSolution(){
+        $query=$this->db->query('SELECT * FROM Solution');
+        $tab = array();
+        foreach ($query->result_array() as $row) {
+            $nom = $row;
+            $tab[] = $nom;
+        }
+        return $tab;
+    }
+
 }
 
 ?>
