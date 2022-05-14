adc1fe3a2c31044598f7e99dd51a085efe17d390
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-05-15 00:11:26 +0300

    Update consequence.php

--- application/views/util/header/consequence.php
+++ application/views/util/header/consequence.php
@@ -1,9 +1,9 @@
-<header class="masthead" style="background-image: url(<?php echo site_url('assets/assets/img')."/".$question["representation"] ?>)">
+<header class="masthead" style="background-image: url(<?php echo site_url('assets/assets/img')."/".$consequence[0]["representation"] ?>)">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
                     <div class="post-heading">
-                        <h1><?php echo $question['question']; ?></h1>
+                        <h1><?php echo $consequence[0]['titre']; ?></h1>
                     </div>
                 </div>
             </div>
