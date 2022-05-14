3cb2f9454b62a234e5acf1c1aac5157e17c299dc
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-05-14 23:45:08 +0300

    Update FrontController.php

--- application/controllers/FrontController.php
+++ application/controllers/FrontController.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('Bonjour');
 
-class Controller extends CI_Controller {
+class FrontController extends CI_Controller {
 
     /**
      * Index Page for this controller.
@@ -28,6 +28,34 @@ class Controller extends CI_Controller {
     //     $data['view'] = 'acceuil';
     //     $this->load->view('template',$data);
     // }
+    public function question(){
+        $data = array();
+        $idQuestion = $_GET['idQuestion'];
+        if($idQuestion == 1){
+            $data['question'] = $this->FirstModel->getQuestionById($idQuestion);
+            $data['cause'] = $this->FirstModel->getCause();
+            $data['title'] = "Cause";
+            $data['header'] = 'util/header/cause';
+            $data['view'] = 'util/view/cause';
+            $this->load->view('frontdetail',$data);
+        }else if($idQuestion == 2){
+            $data['question'] = $this->FirstModel->getQuestionById($idQuestion);
+            $data['consequence'] = $this->FirstModel->getConsequence();
+            $data['title'] = "Conséquence";
+            $data['header'] = 'util/header/consequence';
+            $data['view'] = 'util/view/consequence';
+            $this->load->view('frontdetail',$data);
+        }else if($idQuestion == 3){
+            $data['question'] = $this->FirstModel->getQuestionById($idQuestion);
+            $data['solution'] = $this->FirstModel->getSolution();
+            $data['title'] = "Solution";
+            $data['header'] = 'util/header/solution';
+            $data['view'] = 'util/view/solution';
+            $this->load->view('frontdetail',$data);
+        }else{
+            $this->load->view('errors/html/error_404.php');
+        }
+    }
 }
 
 ?>
