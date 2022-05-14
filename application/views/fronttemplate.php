29906040b74e84b763b8feb80fffde94e6c2c99b
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-05-14 19:27:38 +0300

    Modification fronttemplate

--- application/views/fronttemplate.php
+++ application/views/fronttemplate.php
@@ -16,35 +16,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="<?php echo site_url('css/styles.css') ?>" rel="stylesheet" />
+        <link href="<?php echo site_url('assets/css/styles.css') ?>" rel="stylesheet" />
     </head>
     <body>
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <a class="navbar-brand" href="index.html">E-devyweb</a>
+                <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i class="fas fa-bars"></i>
-                </button>
+                </button> -->
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="#">Sommaire de ce sujet</a></li>
+                        <!-- <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li> -->
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav>  
         <!-- Page Header-->
-        <header class="masthead" style="background-image: url('assets/img/home-bg.jpg')">
+        <header class="masthead" style="background-image: url(<?php echo site_url('assets/assets/img/home-bg.jpg')?>)">
             <div class="container position-relative px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
                         <div class="site-heading">
-                            <h1>Clean Blog</h1>
-                            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                            <h1>Le Réchauffement climatique</h1>
+                            <span class="subheading">causes, impacts et solutions possibles</span>
                         </div>
                     </div>
                 </div>
@@ -57,59 +57,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <!-- Post preview-->
                     <div class="post-preview">
                         <a href="post.html">
-                            <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-                            <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+                            <h2 class="post-title">Qu’est-ce que le réchauffement climatique ?</h2>
+                            <h3 class="post-subtitle">Le globe se réchauffe. Les terres et les océans sont plus chauds aujourd’hui qu’ils ne l’étaient à l’époque où l’on a commencé à tenir des registres, en 1880, et les températures continuent d’augmenter. Cette augmentation de la chaleur est un réchauffement climatique, en un mot.</h3>
+                            <p>
+                            Voici les chiffres exacts,
+                            selon la National Oceanic and Atmospheric Administration (NOAA) : 
+                            Entre 1880 et 1980, la température mondiale annuelle a augmenté à un taux de 0,07 degré Celsius par décennie, en moyenne.
+                            Depuis 1981, le taux d’augmentation s’est accéléré pour atteindre 0,18 degrés C par décennie. 
+                            Cela a conduit à une augmentation globale de 1,4 degrés C de la température moyenne mondiale aujourd’hui par rapport à l’ère préindustrielle. 
+                            En 2019, la température mondiale moyenne sur terre et dans les océans était supérieure de 0,95 °C à la moyenne du XXe siècle. 
+                            Cela fait de 2019 la deuxième année la plus chaude jamais enregistrée, avec un retard de seulement 2016, 2020 bat tous les records quant à elle. 
+                            </p>
                         </a>
                         <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 24, 2022
+                            source
+                            <a href="https://www.jedonnedusens.com/le-rechauffement-climatique-causes-impacts-et-solutions-possibles/">https://www.jedonnedusens.com/le-rechauffement-climatique-causes-impacts-et-solutions-possibles/</a>
                         </p>
                     </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html"><h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2></a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on September 18, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Science has not yet mastered prophecy</h2>
-                            <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on August 24, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="post.html">
-                            <h2 class="post-title">Failure is not an option</h2>
-                            <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!">Start Bootstrap</a>
-                            on July 8, 2022
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <!-- Pager-->
-                    <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div>
-                </div>
             </div>
         </div>
         <!-- Footer-->
@@ -143,7 +107,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 </a>
                             </li>
                         </ul>
-                        <div class="small text-center text-muted fst-italic">Copyright &copy; Your Website 2022</div>
+                        <div class="small text-center text-muted fst-italic">Copyright &copy; RAKOTONIRINA Mendrika Itokiana P13-B 2022</div>
                     </div>
                 </div>
             </div>
@@ -151,6 +115,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="<?php echo site_url('js/scripts.js')?>"></script>
+        <script src="<?php echo site_url('assets/js/scripts.js')?>"></script>
     </body>
 </html>
